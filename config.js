7baedf2dff7458afca312ed14cf657fc51edbc9c
André Calero Valdez <andrecalerovaldez@gmail.com>
2025-12-11 16:39:20 +0100

    moved to new defaults.

---
+++ config.js
@@ -0,0 +1,9 @@
+// Default Christmas Card Messages
+const DEFAULT_MESSAGES = {
+  title: "Frohe Weihnachten!",
+  subtitle: "Erholsame Feiertage und ein glückliches neues Jahr.",
+  message:
+    "Wir hoffen, dass Eure Weihnachtszeit voller Wärme, Lachen und Freude ist und wünschen Euch und Euren Lieben eine wundervolle Weihnachtszeit und ein glückliches, gesundes neues Jahr!",
+  closing: "Herzliche Grüße,",
+  signature: "Euer HCIUSE-Team",
+};
